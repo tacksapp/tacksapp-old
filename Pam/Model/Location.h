04bbd4e32f5b7a63d6aaf19f554e79d0ffdf8d77
Ian Dundas <contact@iandundas.co.uk>
2014-01-28 11:51:19 +0100

    rearranged model classes

--- Pam/Model/Location.h
+++
@@ -1,8 +0,0 @@
-#import <MapKit/MapKit.h>
-#import "_Location.h"
-
-@class PMAnnotation;
-
-@interface Location : _Location<MKAnnotation> {}
-@property(nonatomic, strong) PMAnnotation *annotation;
-@end
